f610cd93023179a196f2a4e0c1323b7f6534daf8
Adrian Borup <adrian@borup.com>
2022-09-01 11:18:24 +0200

    Fix line endings

--- week1/week1.tests/Tests.fs
+++ week1/week1.tests/Tests.fs
@@ -10,9 +10,9 @@ let ``Using max expression should find max of constants 1 and 2`` () =
     Assert.Equal(2, res)
 
 [<Theory>]
-[<InlineData(1, 2, 1)>]  
-[<InlineData(2, 0, 0)>] 
-[<InlineData(13, -13, -13)>] 
+[<InlineData(1, 2, 1)>]
+[<InlineData(2, 0, 0)>]
+[<InlineData(13, -13, -13)>]
 let ``Using min expression should find min of constants`` (a, b, expected) =
     let ex = Prim("min", CstI a, CstI b)
     let res = eval ex []
