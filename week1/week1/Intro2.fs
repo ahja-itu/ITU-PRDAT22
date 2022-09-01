6c7260532427d26233d2f498a1f3b874918281ad
Adrian Borup <adrian@borup.com>
2022-09-01 11:22:40 +0200

    Autoformat Intro2.fs

--- week1/week1/Intro2.fs
+++ week1/week1/Intro2.fs
@@ -6,54 +6,61 @@ module Intro2
 
 (* Association lists map object language variables to their values *)
 
-let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111)];;
+let env =
+    [ ("a", 3)
+      ("c", 78)
+      ("baf", 666)
+      ("b", 111) ]
 
-let emptyenv = []; (* the empty environment *)
+let emptyenv = [] (* the empty environment *)
 
 let rec lookup env x =
-    match env with 
-    | []        -> failwith (x + " not found")
-    | (y, v)::r -> if x=y then v else lookup r x;;
+    match env with
+    | [] -> failwith (x + " not found")
+    | (y, v) :: r -> if x = y then v else lookup r x
 
-let cvalue = lookup env "c";;
+let cvalue = lookup env "c"
 
 
 (* Object language expressions with variables *)
 
-type expr = 
-  | CstI of int
-  | Var of string
-  | Prim of string * expr * expr;;
+type expr =
+    | CstI of int
+    | Var of string
+    | Prim of string * expr * expr
 
-let e1 = CstI 17;;
+let e1 = CstI 17
 
-let e2 = Prim("+", CstI 3, Var "a");;
+let e2 = Prim("+", CstI 3, Var "a")
 
-let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
+let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a")
 
 
 (* Evaluation within an environment *)
 
-let rec eval e (env : (string * int) list) : int =
+let rec eval e (env: (string * int) list) : int =
     match e with
-    | CstI i            -> i
-    | Var x             -> lookup env x 
-    | Prim("+", e1, e2) -> eval e1 env + eval e2 env
-    | Prim("*", e1, e2) -> eval e1 env * eval e2 env
-    | Prim("-", e1, e2) -> eval e1 env - eval e2 env
-    | Prim("max", e1, e2) ->
+    | CstI i -> i
+    | Var x -> lookup env x
+    | Prim ("+", e1, e2) -> eval e1 env + eval e2 env
+    | Prim ("*", e1, e2) -> eval e1 env * eval e2 env
+    | Prim ("-", e1, e2) -> eval e1 env - eval e2 env
+    | Prim ("max", e1, e2) ->
         let v1 = eval e1 env
         let v2 = eval e2 env
         if v1 > v2 then v1 else v2
-    | Prim("min", e1, e2) ->
+    | Prim ("min", e1, e2) ->
         let v1 = eval e1 env
         let v2 = eval e2 env
         if v1 > v2 then v2 else v1
-    | Prim("==", e1, e2) ->
-        if eval e1 env = eval e2 env then 1 else 0 
-    | Prim _            -> failwith "unknown primitive";;
+    | Prim ("==", e1, e2) ->
+        if eval e1 env = eval e2 env then
+            1
+        else
+            0
+    | Prim _ -> failwith "unknown primitive"
 
-let e1v  = eval e1 env;;
-let e2v1 = eval e2 env;;
-let e2v2 = eval e2 [("a", 314)];;
-let e3v  = eval e3 env;;
+let e1v = eval e1 env
+let e2v1 = eval e2 env
+let e2v2 = eval e2 [ ("a", 314) ]
+let e3v = eval e3 env
