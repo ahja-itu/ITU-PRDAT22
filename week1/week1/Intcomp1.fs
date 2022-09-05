de899dda49b7f73dc10bf7ad4b928b4ce9674e5a
Adrian Borup <adrian@borup.com>
2022-09-05 21:27:13 +0200

    Fix errors in Intcomp1.fs

--- week1/week1/Intcomp1.fs
+++ week1/week1/Intcomp1.fs
@@ -153,7 +153,7 @@ let rec nsubst (e: expr) (env: (string * expr) list) : expr =
 
 (* Some expressions with free variables: *)
 
-let e6 = Prim("+", Var "y", Var "z")
+// let e6 = Prim("+", Var "y", Var "z")
 
 let e6s1 = nsubst e6 [ ("z", CstI 17) ]
 
@@ -162,17 +162,17 @@ let e6s2 = nsubst e6 [ ("z", Prim("-", CstI 5, CstI 4)) ]
 let e6s3 = nsubst e6 [ ("z", Prim("+", Var "z", Var "z")) ]
 
 // Shows that only z outside the Let gets substituted:
-let e7 = Prim("+", Let("z", CstI 22, Prim("*", CstI 5, Var "z")), Var "z")
+// let e7 = Prim("+", Let("z", CstI 22, Prim("*", CstI 5, Var "z")), Var "z")
 
 let e7s1 = nsubst e7 [ ("z", CstI 100) ]
 
 // Shows that only the z in the Let rhs gets substituted
-let e8 = Let("z", Prim("*", CstI 22, Var "z"), Prim("*", CstI 5, Var "z"))
+// let e8 = Let("z", Prim("*", CstI 22, Var "z"), Prim("*", CstI 5, Var "z"))
 
 let e8s1 = nsubst e8 [ ("z", CstI 100) ]
 
 // Shows (wrong) capture of free variable z under the let:
-let e9 = Let("z", CstI 22, Prim("*", Var "y", Var "z"))
+// let e9 = Let("z", CstI 22, Prim("*", Var "y", Var "z"))
 
 let e9s1 = nsubst e9 [ ("y", Var "z") ]
 
