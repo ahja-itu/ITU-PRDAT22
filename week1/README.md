24d551a15ae78f96f98b27c0a90a0b49ab27edea
Adrian Borup <adrian@borup.com>
2022-09-05 22:16:07 +0200

    Add Exercise 2.1
    Co-authored-by: Joachim Borup <JoachimBorup@users.noreply.github.com>
    Co-authored-by: Andreas Wachs <andreas@wachs.dk>

--- week1/README.md
+++ week1/README.md
@@ -11,3 +11,5 @@ There are two folders - `week1` containing our solutions and `week1.tests` conta
 - 1.1 has been added to `week1/Intro2.fs`. We have added comments next to our changes.
 - 1.2 has been written to `week1/Exercise_1_2.fs`
 - 1.4 has been written to `week1/Exercise_1_4.java`
+- 2.1 has been written to `week1/Exercise_2.fs` by `// Exercise 2.1` comments
+- 2.2 has been written to `week1/Exercise_2.fs` by `// Exercise 2.2` comments
---
+++ week1/week1/Exercise_2.fs
@@ -0,0 +1,55 @@
+module Exercise_2_1
+
+(* Taken from Intcomp1.fs but Let has been changed *)
+type expr =
+    | CstI of int
+    | Var of string
+    | Let of (string * expr) list * expr // Exercise 2.1
+    | Prim of string * expr * expr
+
+(* Taken directly from Intcomp1.fs *)
+let rec lookup env x =
+    match env with
+    | [] -> failwith (x + " not found")
+    | (y, v) :: r -> if x = y then v else lookup r x
+
+(* Taken from Intcomp1.fs but handling of Let has been changed *)
+let rec eval e (env: (string * int) list) : int =
+    match e with
+    | CstI i -> i
+    | Var x -> lookup env x
+    // Exercise 2.1 - start
+    | Let (xs, ebody) ->
+        List.fold (fun env' (x, erhs) -> (x, eval erhs env') :: env') env xs
+        |> eval ebody
+    // Exercise 2.1 - end
+    | Prim ("+", e1, e2) -> eval e1 env + eval e2 env
+    | Prim ("*", e1, e2) -> eval e1 env * eval e2 env
+    | Prim ("-", e1, e2) -> eval e1 env - eval e2 env
+    | Prim _ -> failwith "unknown primitive"
+
+(* Taken directly from Intcomp1.fs *)
+let rec mem x vs =
+    match vs with
+    | [] -> false
+    | v :: vr -> x = v || mem x vr
+
+(* Taken directly from Intcomp1.fs *)
+let rec union (xs, ys) =
+    match xs with
+    | [] -> ys
+    | x :: xr ->
+        if mem x ys then
+            union (xr, ys)
+        else
+            x :: union (xr, ys)
+
+(* Taken directly from Intcomp1.fs *)
+let rec minus (xs, ys) =
+    match xs with
+    | [] -> []
+    | x :: xr ->
+        if mem x ys then
+            minus (xr, ys)
+        else
+            x :: minus (xr, ys)
