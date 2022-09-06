87d5f1b10321c43626fbeed78d8ef98da1f14ac4
Andreas Wachs <andreas.wachs@motorolasolutions.com>
2022-09-06 11:45:08 +0200

    Push it

--- week1/week1/Exercise_2.fs
+++ week1/week1/Exercise_2.fs
@@ -64,3 +64,41 @@ let rec freevars e : string list =
         List.foldBack (fun (x, erhs) acc -> union (freevars erhs, minus (acc, [ x ]))) xs (freevars ebody)
     // Exercise 2.2 - end
     | Prim (ope, e1, e2) -> union (freevars e1, freevars e2)
+
+
+(* ---------------------------------------------------------------------- *)
+(* ---------- Exercise 2.3, source code taken from Intcom1.fs ----------- *)
+(* ---------------------------------------------------------------------- *)
+
+(* Compilation to target expressions with numerical indexes instead of
+   symbolic variable names.  *)
+
+type texpr = (* target expressions *)
+    | TCstI of int
+    | TVar of int (* index into runtime environment *)
+    | TLet of texpr * texpr (* erhs and ebody                 *)
+    | TPrim of string * texpr * texpr
+
+
+(* Map variable name to variable index at compile-time *)
+
+let rec getindex vs x =
+    match vs with
+    | [] -> failwith "Variable not found"
+    | y :: yr -> if x = y then 0 else 1 + getindex yr x
+
+(* Compiling from expr to texpr *)
+
+let rec tcomp (e: expr) (cenv: string list) : texpr =
+    match e with
+    | CstI i -> TCstI i
+    | Var x -> TVar(getindex cenv x)
+    | Let (xs, ebody) ->
+        // List.fold (fun env' (x, erhs) -> (x, eval erhs env') :: env') env xs        
+        let res = List.foldBack (fun (x, erhs) (ebody', cenv') ->
+            (TLet (tcomp erhs cenv', tcomp ebody' (x :: cenv')), x :: cenv')) xs (ebody, cenv)
+
+
+        let cenv1 = x :: cenv
+        TLet(tcomp erhs cenv, tcomp ebody cenv1)
+    | Prim (ope, e1, e2) -> TPrim(ope, tcomp e1 cenv, tcomp e2 cenv)
