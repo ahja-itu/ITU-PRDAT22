fbbcca4bf70d9c7917071aa2cea58d6625000579
Andreas Wachs <andreas.wachs@motorolasolutions.com>
2022-09-06 12:13:21 +0200

    Make a test for ex 2.3

--- week1/week1.tests/Exercise_2_Tests.fs
+++ week1/week1.tests/Exercise_2_Tests.fs
@@ -10,3 +10,12 @@ let ``Free variable is detected`` () =
     // For some reason Assert.Equal is dumb here
     let isExpected = [ "a" ] = freevars e
     Assert.True(isExpected)
+
+[<Fact>]
+let ``tcomp allows for sequential let expressions`` () =
+    let e = Let([ ("x", CstI 5); ("y", CstI 10) ], Prim("+", Var("x"), Var("y")))
+
+    let res = tcomp e []
+
+    let expected = TLet (TCstI 5, TLet (TCstI 10, TPrim ("+", TVar 1, TVar 0)))
+    Assert.Equal(expected, res)
